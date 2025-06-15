31535d2dbcf7e434344bc973cad386e634619e90
ZRiss1980 <zac@zbrewtech.com>
2025-06-15 14:38:37 -0600

    Finalize logout button in AppLayout

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,20 +1,18 @@
-// /src/components/AppLayout.jsx
 import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import NavBar from "./NavBar";
 import { useAuth } from "../Auth/AuthProvider";
 
-
 export default function AppLayout() {
   const isLogin = useLocation().pathname === "/login";
-  const { logout } = useAuth(); 
+  const { logout } = useAuth();
 
   return (
     <>
       {!isLogin && <NavBar />}
       <main>
-        <Outlet />
         <button onClick={logout}>Sign Out</button>
+        <Outlet />
       </main>
     </>
   );
